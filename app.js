34491a615a082051e84372e606af7bc8fd1a5e31
0Tristan0 <goehrint@oregonstate.edu>
2025-01-06 18:08:46 -0800

    Re-arranged the format of the website. I also formmatted the text in a betetr way.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const context = canvas.getContext("2d");
-const frameCount = 225;
+const frameCount = 191;
 
 const currentFrame = (index) => `./Animation-Photos/${(index + 1).toString()}.jpg`;
 const images = [];
@@ -41,62 +41,6 @@ gsap.fromTo(".hi-text", {opacity:1},
   },
   onComplete: () => {
     gsap.to(".hi-text", {opacity:0});
-  }
-}
-);
-
-gsap.fromTo(".EmailImg", {opacity:0},
-  {opacity:1,
-  scrollTrigger: {
-    // fades the text
-    scrub: true,
-    start: "68%",
-    end: "100%",
-  },
-  onComplete: () => {
-    gsap.to(".EmailImg", {opacity:0});
-  }
-}
-);
-
-gsap.fromTo(".about-me", {opacity:0},
-  {opacity:1,
-  scrollTrigger: {
-    // fades the text
-    scrub: true,
-    start: "7%",
-    end: "15%",
-  },
-  onComplete: () => {
-    gsap.to(".about-me", {opacity:0});
-  }
-}
-);
-
-gsap.fromTo(".my-paragraph", {opacity:0},
-  {opacity:1,
-  scrollTrigger: {
-    // fades the text
-    scrub: true,
-    start: "13%",
-    end: "24%",
-  },
-  onComplete: () => {
-    gsap.to(".my-paragraph", {opacity:0});
-  }
-}
-);
-
-gsap.fromTo(".download-resume", {opacity:0},
-  {opacity:1,
-  scrollTrigger: {
-    // fades the text
-    scrub: true,
-    start: "13%",
-    end: "24%",
-  },
-  onComplete: () => {
-    gsap.to(".download-resume", {opacity:0});
   }
 }
 );
